b73a449446aef17c6e7d1a2489d529e1e6f6496d
luizaugustopa <00001101809310sp@al.educacao.sp.gov.br>
2024-11-20 18:02:24 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,36 @@
+criaCartao (
+   'Matemática',
+    'O que é uma equação?',
+    'É a igualdade que relaciona uma operação matemática aplicada em alguns números com o seu resultado.'
+)
+
+criaCartao (
+    'Matemática',
+     'O que é um teorema de pitágonas?',
+     ' Determina que, em qualquer triângulo retângulo, o quadrado da hipotenusa é igual à soma dos quadrados dos catetos.'
+ )
+
+ criaCartao (
+    'Matemática',
+     'O que é função?',
+     'É uma regra que relaciona cada elemento de um conjunto a um único elemento de outro.'
+ )
+
+ criaCartao (
+    'Fisíca',
+     'O que é atômo?',
+     'O átomo é a unidade básica de construção da matéria. É a menor porção de um elemento químico que mantém sua identidade.'
+ )
+
+ 
+ criaCartao (
+    'Fisíca',
+     'O que é elétron?',
+     'O elétron é uma partícula presente na estrutura do átomo e que possui carga elétrica negativa.'
+ )
+
+ criaCartao (
+    'Fisíca',
+     'Quem foi Galileu Galilei?',
+     'Nascido em 1564, na Itália, Galileu Galilei é considerado o pai da física moderna experimental, e o grande precursor da astronomia telescópica. Ainda no século XVII, seus experimentos nas áreas exatas revolucionaram a história das ciências.'
+ )
